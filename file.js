c829dd91a0b5301f44f8e69abf1283a84b477407
jimbobding <jimmygrahamdavid@gmail.com>
2022-09-14 13:54:44 +0000

    added refresh page function

--- file.js
+++ file.js
@@ -1,14 +1,31 @@
 
 function weight() {
     let kg = (document.getElementById("digit").value)
-   
     
-    if (!isNaN(kg) &&( kg.length === 4 || kg.length === 6) )  { document.getElementById('myWeight').innerHTML = alert(kg) } else { document.getElementById('myWeight').innerHTML = 'no'}
-    
-
-
+    if (!isNaN(kg) && ( kg.length === 4 || kg.length === 6) )  { document.getElementById('pic').style.display = 'block'  && (document.getElementById('button').style.display = 'none') && (document.getElementById('button2').style.display = 'block')} 
+     else  {document.getElementById('pic2').style.display = 'block'  && (document.getElementById('button').style.display = 'none') && (document.getElementById('button2').style.display = 'block')}  
    
-    console.log(typeof(kg))
-    return kg
 }
+
+function display() {
+    let kg = (document.getElementById("digit").value)
+    if (!isNaN(kg) &&( kg.length === 4 || kg.length === 6) )  {(document.getElementById('myWeight').innerHTML = alert(kg))} else { document.getElementById('myWeight').innerHTML = alert('nah')} 
+}
+
+
+
+function refreshPage(){
+    window.location.reload();
+} 
+
+
+// document.getElementById("myForm").onreset = function() {myFunction()};
+
+// function reset() {
+//     if (document.getElementById('pic').style.display = 'block') {document.getElementById('pic2').style.display = 'none'} else {document.getElementById('pic').style.display = 'block'}
+// }
+
+// function reset2() {
+//     if (document.getElementById('pic2').style.display = 'block') {document.getElementById('pic').style.display = 'none'} else {document.getElementById('pic2').style.display = 'block'}
+// }
 
